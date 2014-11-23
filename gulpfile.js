b28072404fa44c66fe9f8dda253403616c28720b
Anil Bhanushali <b.anil@better-life.co.in>
2014-11-23 19:27:03 +0530

    changed the structure and updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,306 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var del = require('del');
+var merge = require('merge-stream');
+var glob = require('glob');
 var plug = require('gulp-load-plugins')();
+var paths = require('./gulp.config.json');
+var plato = require('plato');
+var colors = plug.util.colors;
+var log = plug.util.log;
+var connect = require('gulp-connect');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
+var serverPort = 8000;
+var livereloadPort = 35777;
 
-gulp.task('default', ['sass', 'templatecache', 'js']);
+gulp.task('analyze', function() {
+    log('Analyzing source with JSHint, JSCS, and Plato');
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    var jshint = analyzejshint([].concat(paths.js));
+    var jscs = analyzejscs([].concat(paths.js));
+
+    startPlatoVisualizer();
+
+    return merge(jshint, jscs);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
-
-gulp.task('templatecache', ['sass'], function(cb) {
-    gutil.log('Creating an AngularJS $templateCache');
-
-    gulp.src('./www/partials/**/*.html')
+gulp.task('templatecache', function() {
+    log('Creating an AngularJS $templateCache');
+    
+    return gulp.src(paths.htmltemplates)
+      .pipe(plug.minifyHtml({
+        empty:true
+      }))
       .pipe(plug.angularTemplatecache('templates.js', {
           module: 'myapp',
           standalone: false,
           root: 'partials/'
       }))
-      .pipe(gulp.dest('./www/js/'))
-      .on('end', cb);
+      .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('js', ['templatecache'], function() {
-    gutil.log('Bundling, minifying, and copying the partial\'s JavaScript');
+    log('Bundling, minifying, and copying the partial\'s JavaScript');
 
-    var source = [
-      "./www/partials/**/*.js",
-      "./www/js/templates.js",
-      "!./www/js/*partials.min.js*"
-      ];
+    var source = [].concat(paths.js, paths.build+'templates.js');
 
     return gulp
         .src(source)
-        .pipe(plug.concat('partials.min.js'))
+        .pipe(plug.concat('all.min.js'))
+        .pipe(plug.ngAnnotate({
+            add: true,
+            single_quotes: true
+        }))
         .pipe(plug.bytediff.start())
-        .pipe(plug.uglify({mangle: true}))
-        .pipe(plug.bytediff.stop())
-        .pipe(gulp.dest('./www/js/'));
+        .pipe(plug.uglify({
+            mangle: true
+        }))
+        .pipe(plug.bytediff.stop(bytediffFormatter))
+        .pipe(gulp.dest(paths.build))
+        .pipe(connect.reload());
 });
+
+/**
+ * Copy the Vendor JavaScript
+ * @return {Stream}
+ */
+gulp.task('vendorjs', function() {
+    log('Bundling, minifying, and copying the Vendor JavaScript');
+
+    return gulp.src(paths.vendorjs)
+        .pipe(plug.concat('vendor.min.js'))
+        .pipe(plug.bytediff.start())
+        .pipe(plug.uglify())
+        .pipe(plug.bytediff.stop(bytediffFormatter))
+        .pipe(gulp.dest(paths.build));
+});
+
+/**
+ * Minify and bundle the CSS
+ * @return {Stream}
+ */
+gulp.task('css', function() {
+    log('Bundling, minifying, and copying the app\'s CSS');
+
+    return gulp.src(paths.css)
+        .pipe(plug.concat('all.min.css')) // Before bytediff or after
+        .pipe(plug.autoprefixer('last 2 version', '> 5%'))
+        .pipe(plug.bytediff.start())
+        .pipe(plug.minifyCss({}))
+        .pipe(plug.bytediff.stop(bytediffFormatter))
+        .pipe(gulp.dest(paths.build + 'css'))
+        .pipe(connect.reload());
+});
+
+/**
+ * Minify and bundle the Vendor CSS
+ * @return {Stream}
+ */
+gulp.task('vendorcss', function() {
+    log('Compressing, bundling, copying vendor CSS');
+
+    var vendorFilter = plug.filter(['**/*.css']);
+
+    return gulp.src(paths.vendorcss)
+        .pipe(vendorFilter)
+        .pipe(plug.concat('vendor.min.css'))
+        .pipe(plug.bytediff.start())
+        .pipe(plug.minifyCss({}))
+        .pipe(plug.bytediff.stop(bytediffFormatter))
+        .pipe(gulp.dest(paths.build + 'css'));
+});
+
+/**
+ * Copy fonts
+ * @return {Stream}
+ */
+gulp.task('fonts', function() {
+    var dest = paths.build + 'fonts';
+    log('Copying fonts');
+    return gulp
+        .src(paths.fonts)
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
+});
+
+/**
+ * Compress images
+ * @return {Stream}
+ */
+gulp.task('images', function() {
+    var dest = paths.build + 'images';
+    log('Compressing, caching, and copying images');
+    return gulp
+        .src(paths.images)
+        .pipe(plug.cache(plug.imagemin({
+            optimizationLevel: 3
+        })))
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());;
+});
+
+/**
+ * Inject all the files into the new index.html
+ * rev, but no map
+ * @return {Stream}
+ */
+gulp.task('rev-and-inject', ['js', 'vendorjs', 'css', 'vendorcss'], function() {
+    log('Rev\'ing files and building index.html');
+
+    var minified = paths.build + '**/*.min.*';
+    var index = paths.client + 'index.html';
+    var minFilter = plug.filter(['**/*.min.*', '!**/*.map']);
+    var indexFilter = plug.filter(['index.html']);
+
+    var stream = gulp
+        // Write the revisioned files
+        .src([].concat(minified, index)) // add all built min files and index.html
+     //   .pipe(minFilter) // filter the stream to minified css and js
+     //   .pipe(plug.rev()) // create files with rev's
+     //   .pipe(gulp.dest(paths.build)) // write the rev files
+     //   .pipe(minFilter.restore()) // remove filter, back to original stream
+
+    // inject the files into index.html
+    .pipe(indexFilter) // filter to index.html
+    .pipe(inject('css/vendor.min.css', 'inject-vendor'))
+        .pipe(inject('css/all.min.css'))
+        .pipe(inject('vendor.min.js', 'inject-vendor'))
+        .pipe(inject('all.min.js'))
+        .pipe(gulp.dest(paths.build)) // write the rev files
+    .pipe(indexFilter.restore()) // remove filter, back to original stream
+
+    // replace the files referenced in index.html with the rev'd files
+    //.pipe(plug.revReplace()) // Substitute in new filenames
+    .pipe(gulp.dest(paths.build)); // write the index.html file changes
+    //.pipe(plug.rev.manifest()) // create the manifest (must happen last or we screw up the injection)
+    //.pipe(gulp.dest(paths.build)); // write the manifest
+
+    function inject(path, name) {
+        var pathGlob = paths.build + path;
+        var options = {
+            ignorePath: paths.build.substring(1),
+            addRootSlash : false,
+            read: false
+        };
+        if (name) {
+            options.name = name;
+        }
+        return plug.inject(gulp.src(pathGlob), options);
+    }
+});
+
+/**
+ * Build the optimized app
+ * @return {Stream}
+ */
+gulp.task('default', ['rev-and-inject', 'images', 'fonts','watch','connect'], function() {
+    log('Building the optimized app');
+
+    return gulp.src('').pipe(plug.notify({
+        onLast: true,
+        message: 'Deployed code!'
+    }));
+});
+
+/**
+ * Remove all files from the build folder
+ * One way to run clean before all tasks is to run
+ * from the cmd line: gulp clean && gulp build
+ * @return {Stream}
+ */
+gulp.task('clean', function(cb) {
+    log('Cleaning: ' + plug.util.colors.blue(paths.build));
+
+    var delPaths = [].concat(paths.build, paths.report);
+    del(delPaths, cb);
+});
+
+gulp.task('connect', function(){
+    connect.server({
+      root: paths.build,
+      port: serverPort,
+      livereload : true
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.htmltemplates, ['js']);
+    gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.fonts, ['fonts']);
+});
+
+
+
+/**
+ * Start Plato inspector and visualizer
+ */
+function startPlatoVisualizer() {
+    log('Running Plato');
+
+    var files = glob.sync('./html/js/**/*.js');
+
+    var options = {
+        title: 'Plato Inspections Report',
+        exclude: excludeFiles
+    };
+    var outputDir = './report/plato';
+
+    plato.inspect(files, outputDir, options, platoCompleted);
+
+    function platoCompleted(report) {
+        var overview = plato.getOverviewReport(report);
+        log(overview.summary);
+    }
+}
+/**
+ * Execute JSHint on given source files
+ * @param  {Array} sources
+ * @param  {String} overrideRcFile
+ * @return {Stream}
+ */
+function analyzejshint(sources, overrideRcFile) {
+    var jshintrcFile = overrideRcFile || './.jshintrc';
+    log('Running JSHint');
+    log(sources);
+    return gulp
+        .src(sources)
+        .pipe(plug.jshint(jshintrcFile))
+        .pipe(plug.jshint.reporter('jshint-stylish'));
+}
+
+/**
+ * Execute JSCS on given source files
+ * @param  {Array} sources
+ * @return {Stream}
+ */
+function analyzejscs(sources) {
+    log('Running JSCS');
+    return gulp
+        .src(sources)
+        .pipe(plug.jscs('./.jscsrc'));
+}
+
+/**
+ * Formatter for bytediff to display the size changes after processing
+ * @param  {Object} data - byte data
+ * @return {String}      Difference in bytes, formatted
+ */
+function bytediffFormatter(data) {
+    var difference = (data.savings > 0) ? ' smaller.' : ' larger.';
+    return data.fileName + ' went from ' +
+        (data.startSize / 1000).toFixed(2) + ' kB to ' + (data.endSize / 1000).toFixed(2) + ' kB' +
+        ' and is ' + formatPercent(1 - data.percent, 2) + '%' + difference;
+}
+
+/**
+ * Format a number as a percentage
+ * @param  {Number} num       Number to format as a percent
+ * @param  {Number} precision Precision of the decimal
+ * @return {Number}           Formatted perentage
+ */
+function formatPercent(num, precision) {
+    return (num * 100).toFixed(precision);
+}
+
